8250a5554f7eb79f3036f993b7e408878b7616e2
Swathi Reddy Konatham <swathikonatham@users.github.com>
2020-09-02 15:52:52 -0500

    Sorting startegies on sorted data and input file
    Implementation of Sorting strategies on sorted data

--- Lab1/WorldPopulation.java
+++ Lab1/WorldPopulation.java
@@ -9,42 +9,43 @@ public class WorldPopulation {
 
 	SortStrategy sortStrategy;
 	long[] population = new long[13484]; // Cheating because we know number of records!!
-		
-	// Lab Exercise:  After creating some strategy classes -- set the default strategy here.
-	public WorldPopulation(){
-		sortStrategy = new BubbleSort(); // Set the default strategy here.	
+
+	// Lab Exercise: After creating some strategy classes -- set the default
+	// strategy here.
+	public WorldPopulation() {
+		sortStrategy = new BubbleSort(); // Set the default strategy here.
 	}
-	
-	public void readInputFile(){
+
+	public void readInputFile() {
 		population = readPopulationFile("WorldPopulation.csv");
 	}
-	
-	public void setStrategy(SortStrategy strategy){
+
+	public void setStrategy(SortStrategy strategy) {
 		sortStrategy = strategy;
 		sortStrategy.sort(population);
 	}
-	
-	// Lab Exercise:  Read in the WorldPopulation.csv
+
+	// Lab Exercise: Read in the WorldPopulation.csv
 	// Extract ONLY the numbers and store them into population[]
-	public long[] readPopulationFile(String fileName){
+	public long[] readPopulationFile(String fileName) {
 		int i = 0;
 		try {
 			FileReader inputFile = new FileReader(fileName);
 			BufferedReader reader = new BufferedReader(inputFile);
 			String line;
 			// reads each line
-			while((line = reader.readLine()) != null) {
+			while ((line = reader.readLine()) != null) {
 				StringTokenizer tokenizer = new StringTokenizer(line, ",");
-				while(tokenizer.hasMoreTokens()) {
-					//read first column - Country
+				while (tokenizer.hasMoreTokens()) {
+					// read first column - Country
 					tokenizer.nextToken();
-					//read second column - year
+					// read second column - year
 					tokenizer.nextToken();
-					//read third column - population count and parse it to Long
+					// read third column - population count and parse it to Long
 					long count = Long.parseLong(tokenizer.nextToken());
 					population[i++] = count;
 				}
-				
+
 			}
 			reader.close();
 		} catch (Exception e) {
@@ -52,33 +53,55 @@ public class WorldPopulation {
 		}
 		return population;
 	}
-	
-	// Lab Exercise.  Complete this method.
+
+	// Lab Exercise. Complete this method.
 	// Delegate sorting to the strategy object
-	public void sortPopulation(){
+	public void sortPopulation() {
 		sortStrategy.sort(population);
 		System.out.println("The sorting method used is: " + sortStrategy.strategyName());
 		System.out.println("The total time taken for sorting is: " + sortStrategy.getSortTime() + " milliseconds.");
-		System.out.println();
-		
+		//System.out.println();
+
 	}
-	
-	public void computeTotalPopulation(){
+
+	public void computeTotalPopulation() {
 		System.out.println("dd");
 	}
-	
+
 	// Experiment with various strategies.
 	// Create 3 strategies -- Bubble, insertion, and selection sort.
 	public static void main(String[] args) {
 		WorldPopulation worldPopulation = new WorldPopulation();
-		worldPopulation.readInputFile();
 		
-		//Invoke Bubble Sort
+		System.out.println("Implementation of Sorting strategies using input file:");
+		// Invoke Bubble Sort
+		worldPopulation.readInputFile();
 		worldPopulation.setStrategy(new BubbleSort());
 		worldPopulation.sortPopulation();
-		
-		//Invoke Selection Sort
+
+		// Invoke Selection Sort
+		worldPopulation.readInputFile();
 		worldPopulation.setStrategy(new SelectionSort());
+		worldPopulation.sortPopulation();
+
+		// Invoke Insertion Sort
+		worldPopulation.readInputFile();
+		worldPopulation.setStrategy(new InsertionSort());
+		worldPopulation.sortPopulation();
+		
+		System.out.println();
+		System.out.println("Implementation of Sorting strategies on sorted data:");
+
+		// Invoke Bubble Sort
+		worldPopulation.setStrategy(new BubbleSort());
+		worldPopulation.sortPopulation();
+
+		// Invoke Selection Sort
+		worldPopulation.setStrategy(new SelectionSort());
+		worldPopulation.sortPopulation();
+
+		// Invoke Insertion Sort
+		worldPopulation.setStrategy(new InsertionSort());
 		worldPopulation.sortPopulation();
 	}
 
